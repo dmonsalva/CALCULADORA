0976c6840294d3318c00012afc4f5029f39f2299
DAVID MONSALVA REYES <cobooz.co@gmail.com>
2025-05-19 01:35:04 +0000

    Corrigiendo y subiendo archivos actualizados

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,23 +1,5 @@
 import numpy as np
 
-<<<<<<< HEAD
-def calculate(lista):
-    if len(lista) != 9:
-        raise ValueError("La lista debe contener nueve números")
-    
-    matriz = np.array(lista).reshape(3, 3)
-
-    estadisticas = {
-        'mean': [matriz.mean(axis=0).tolist(), matriz.mean(axis=1).tolist(), matriz.mean().tolist()],
-        'variance': [matriz.var(axis=0).tolist(), matriz.var(axis=1).tolist(), matriz.var().tolist()],
-        'standard deviation': [matriz.std(axis=0).tolist(), matriz.std(axis=1).tolist(), matriz.std().tolist()],
-        'max': [matriz.max(axis=0).tolist(), matriz.max(axis=1).tolist(), matriz.max().tolist()],
-        'min': [matriz.min(axis=0).tolist(), matriz.min(axis=1).tolist(), matriz.min().tolist()],
-        'sum': [matriz.sum(axis=0).tolist(), matriz.sum(axis=1).tolist(), matriz.sum().tolist()]
-    }
-
-    return estadisticas
-=======
 def calculate(numbers):
     if len(numbers) != 9:
         raise ValueError("La lista debe contener nueve números")
@@ -34,4 +16,4 @@ def calculate(numbers):
     }
 
     return calculations
->>>>>>> 45d73d6 (Subiendo versión modificada del visualizador de series de tiempo)
+
